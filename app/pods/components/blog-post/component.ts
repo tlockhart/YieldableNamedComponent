3c3a8f7894db7b27e18651cf7771858a1629ed5b
Tony Lockhart <tony.lockhart@ymail.com>
2021-06-20 17:12:54 -0400

    Updated example 1-4

--- app/pods/components/blog-post/component.ts
+++ app/pods/components/blog-post/component.ts
@@ -12,5 +12,20 @@ interface BlogPostArgs {
 }
 
 export default class BlogPost extends Component<BlogPostArgs> {
-
+    constructor(owner: unknown, args: BlogPostArgs){
+        super(owner, args);
+        console.log(`BlogPost EditStyle: ${this.args.editStyle}`);
+        console.log(`BlogPost Author: ${this.args.author}`);
+    }
+    get author() {
+        return this.args.author;
+    }
+    get title() {
+        console.log(`Title: ${this.args.title}`)
+        return this.args.title;
+    }
+    get editStyle() {
+        console.log(`EditStyle: ${this.args.editStyle}`)
+        return this.args.editStyle;
+    }
 }
--- app/pods/components/mystyle/component.ts
+++ app/pods/components/mystyle/component.ts
@@ -1,15 +1,17 @@
 import Component from '@glimmer/component';
 
-interface MarkdownStyleArgs {
+interface MystyleArgs {
     indexRouteColor: string;
     indexRouteAuthor: string;
-    indexRouteBody: string;
 }
 
-export default class MarkdownStyle extends Component<MarkdownStyleArgs> {
-    constructor(owner: unknown, args: MarkdownStyleArgs) {
+export default class Mystyle extends Component<MystyleArgs> {
+    constructor(owner: unknown, args: MystyleArgs) {
         super(owner, args);
         console.log("Called markdownStyle");
         console.log(`Color: ${this.args.indexRouteColor}`);
+    }
+    get color() {
+        return "red";
     }
 }
