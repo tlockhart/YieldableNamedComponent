6b9099ce20ff12be1a19d929a01045ee5ab19cf2
Tony Lockhart <tony.lockhart@ymail.com>
2021-06-20 13:42:17 -0400

    v3.16.0...v3.24.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'contextual-components/app';
+import config from 'contextual-components/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
